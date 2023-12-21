549b492cacf6217687ad3243ce48a628b2643383
Pavel Borisov <pavellbor@gmail.com>
2023-12-21 12:13:25 +0300

    feat: Добавлена команда `VersionCommand`

---
+++ src/cli/commands/version.command.ts
@@ -0,0 +1,48 @@
+import { resolve } from 'node:path';
+import { Command } from './command.interface';
+import { readFileSync } from 'node:fs';
+
+type PackageJSONConfig = {
+  version: string;
+};
+
+function isPackageJSONConfig(value: unknown): value is PackageJSONConfig {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    !Array.isArray(value) &&
+    Object.hasOwn(value, 'version')
+  );
+}
+
+export class VersionCommand implements Command {
+  constructor(private filePath: string = './package.json') {}
+
+  public getName(): string {
+    return '--version';
+  }
+
+  public execute(..._parameters: string[]): void {
+    try {
+      const version = this.readVersion();
+      console.info(version);
+    } catch (error: unknown) {
+      console.error(`Failed to read version from ${this.filePath}`);
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  private readVersion(): string {
+    const jsonContent = readFileSync(resolve(this.filePath), 'utf-8');
+    const importedContent = JSON.parse(jsonContent);
+
+    if (!isPackageJSONConfig(importedContent)) {
+      throw new Error('Failed to parse json content.');
+    }
+
+    return importedContent.version;
+  }
+}
