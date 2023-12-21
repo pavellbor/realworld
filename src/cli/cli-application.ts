99e4c660cac4ec7e4a56be30a3530b36ce800085
Pavel Borisov <pavellbor@gmail.com>
2023-12-21 21:49:44 +0300

    feat: Добавлен метод `processCommand` в класс `CLIApplication`

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,24 @@
+import { CommandParser } from './command-parser';
 import { Command } from './commands/command.interface';
 
 type CommandCollection = Record<string, Command>;
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(private readonly defaultCommand: string = '--help') {}
+
+  private getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  private getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+
+    return this.commands[this.defaultCommand];
+  }
 
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -15,5 +30,14 @@ export class CLIApplication {
 
       this.commands[commandName] = command;
     });
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName];
+
+    command.execute(...commandArguments);
   }
 }
