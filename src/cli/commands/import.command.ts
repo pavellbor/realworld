f3e062fccbd53fb8f6127e262cedb03efa6a44ad
Pavel Borisov <pavellbor@gmail.com>
2023-12-28 11:27:29 +0300

    feat: Добавлено чтение файла в команду `--import`

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,11 +1,25 @@
+import { TSVFileReader } from '../../shared/libs/file-reader';
 import { Command } from './command.interface';
 
 export class ImportCommand implements Command {
   public getName(): string {
-      return '--import'
+    return '--import';
   }
 
   public execute(...parameters: string[]): void {
-      // Чтение файла
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
   }
 }
