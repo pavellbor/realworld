e32b2f63bda674f7dc524ba2493fcc1caeb5c0f0
Pavel Borisov <pavellbor@gmail.com>
2023-12-28 11:22:48 +0300

    feat: Добавлен класс `TSVFileReader`

---
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -0,0 +1,53 @@
+import { readFileSync } from 'node:fs';
+import { FileReader } from './file-reader.interface';
+import { Article, ArticleType } from '../../types';
+
+export class TSVFileReader implements FileReader {
+  private rawData = '';
+
+  constructor(private readonly fileName: string) {}
+
+  public read(): void {
+    this.rawData = readFileSync(this.fileName, { encoding: 'utf-8' });
+  }
+
+  public toArray(): Article[] {
+    if (!this.rawData) {
+      throw new Error('File was not read');
+    }
+
+    return this.rawData
+      .split(/(?<!\\)\\n/)
+      .filter((row) => row.trim().length > 0)
+      .map((line) => line.split('\t'))
+      .map(
+        ([
+          title,
+          description,
+          body,
+          image,
+          type,
+          tags,
+          createdDate,
+          email,
+          username,
+          avatarPath,
+        ]) => {
+          return {
+            title,
+            description,
+            body,
+            postDate: new Date(createdDate),
+            image,
+            tags: tags.split(';').map((name) => ({ name })),
+            type: ArticleType[type as 'Private' | 'Public'],
+            author: {
+              email,
+              username,
+              avatarPath,
+            },
+          };
+        },
+      );
+  }
+}
