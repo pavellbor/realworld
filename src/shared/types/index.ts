efab8a199eb13610afa670fcbb62d215db44af88
Pavel Borisov <pavellbor@gmail.com>
2024-01-01 16:55:50 +0300

    feat: Добавлен тип MockServerData

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export * from './article-type.enum';
 export * from './article.type';
 export * from './category.type';
 export * from './user.type';
+export * from './mock-server-data.type';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,9 @@
+export type MockServerData = {
+  titles: string[];
+  bodies: string[];
+  images: string[];
+  categories: string[];
+  emails: string[];
+  avatars: string[];
+  usernames: string[];
+};
