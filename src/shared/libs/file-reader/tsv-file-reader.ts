d88574e33d6c327011f22a818d3747bd899efe23
Pavel Borisov <pavellbor@gmail.com>
2024-01-03 15:21:32 +0300

    feat: Добавлены потоки в `TSVFileReader`

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,12 +1,36 @@
-import { readFileSync } from 'node:fs';
+import EventEmitter from 'node:events';
 import { FileReader } from './file-reader.interface.js';
+import { createReadStream } from 'node:fs';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly fileName: string) {}
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly fileName: string) {
+    super();
+  }
 
-  public read(): void {
-    this.rawData = readFileSync(this.fileName, { encoding: 'utf-8' });
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.fileName, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completedRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completedRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
