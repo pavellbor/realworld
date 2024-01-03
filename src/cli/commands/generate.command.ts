933e4de958049461d3a46d00d662df3a68e06192
Pavel Borisov <pavellbor@gmail.com>
2024-01-03 12:35:00 +0300

    feat: Добавлен запрос на получение данных в `GenerateCommand`

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,15 +1,34 @@
-import { Command } from './command.interface';
+import got from 'got';
+import { MockServerData } from '../../shared/types/index.js';
+import { Command } from './command.interface.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string): Promise<void> {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const articleCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера
-    // Формирование постов
+    try {
+      this.load(url);
+    } catch (error: unknown) {
+      console.error("Can't generate data");
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
