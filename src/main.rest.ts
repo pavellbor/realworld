eef0ae5c264dadad69c774f378f3a66176818b07
Pavel Borisov <pavellbor@gmail.com>
2024-01-06 12:00:43 +0300

    feat: Добавлен `Config` в `RestApplication`

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { RestApplication } from './rest/indext.js';
+import { RestConfig } from './shared/libs/config/index.js';
 import { PinoLogger } from './shared/libs/logger/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,9 +1,14 @@
+import { Config } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config,
+  ) {}
 
   public async init() {
     this.logger.info('Application initilization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
