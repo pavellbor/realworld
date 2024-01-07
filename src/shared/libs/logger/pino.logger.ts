ecad88347b0c69d0bcd48050981b41e117732e35
Pavel Borisov <pavellbor@gmail.com>
2024-01-07 12:37:12 +0300

    feat: Настроен контейнер на создание синглтонов

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -29,6 +29,8 @@ export class PinoLogger implements Logger {
     });
 
     this.logger = pino({}, multiTransport);
+
+    this.logger.info('Logger created!');
   }
 
   public debug(message: string, ...args: unknown[]): void {
