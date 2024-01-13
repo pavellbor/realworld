0524b1d392c6f3c7099558c060eef96a27b02b97
Pavel Borisov <pavellbor@gmail.com>
2024-01-13 14:12:56 +0300

    feat: Добавит определение модели и схемы в UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,7 +1,24 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({
+    unique: true,
+    required: true,
+  })
   email: string;
+
+  @prop({
+    unique: true,
+    required: false,
+  })
   username: string;
+
+  @prop({
+    required: false,
+    default: '',
+  })
   avatarPath: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
