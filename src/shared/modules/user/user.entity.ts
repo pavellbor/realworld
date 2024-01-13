24f8a84f56c16d718bd1d202b777505a5ef30bd3
Pavel Borisov <pavellbor@gmail.com>
2024-01-13 16:19:24 +0300

    feat: Добавлено поле `password` в UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,26 +1,53 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
-export interface UserEntity extends defaultClasses.Base, defaultClasses.TimeStamps {}
+export interface UserEntity extends defaultClasses.Base {}
 
-export class UserEntity implements User {
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+  },
+})
+export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({
     unique: true,
     required: true,
   })
-  email: string;
+  public email: string;
 
   @prop({
     unique: true,
     required: false,
   })
-  username: string;
+  public username: string;
 
   @prop({
     required: false,
     default: '',
   })
-  avatarPath: string;
+  public avatarPath: string;
+
+  @prop({
+    required: true,
+  })
+  private password?: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.username = userData.username;
+    this.avatarPath = userData.avatarPath;
+  }
+
+  public setPassword(password: string, salt: string): void {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
