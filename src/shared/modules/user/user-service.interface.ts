d4f6409f36b0d68f2a0ae2f1edb149d5d97d1da6
Pavel Borisov <pavellbor@gmail.com>
2024-01-14 12:42:31 +0300

    feat: Имплементированы все методы `DefaultUserService`

--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -4,6 +4,6 @@ import { CreateUserDto } from './dto/create-user.dto.js';
 
 export interface UserService {
   create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
-  findByEmail(email: string): Promise<DocumentType<UserEntity>>;
+  findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
   findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
 }
